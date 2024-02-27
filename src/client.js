eae6b547287468299c9de54f35a9a7725bed06eb
Arpan Solanki <solankiarpan@gmail.com>
2024-02-27 15:50:41 -0500

    feat: adding client.js

--- src/client.js
+++ src/client.js
@@ -1,20 +1,25 @@
-const { Client } = require('pg'); 
+const { Client } = require('pg');
 const pgclient = new Client({
-    host: process.env.POSTGRES_HOST, 
-    port: process.env.POSTGRES_PORT, 
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
     user: 'postgres',
     password: 'postgres',
     database: 'postgres'
-}); 
-
-pgclient.connect();
-
-console.log("here")
-
-pgclient.query('SELECT NOW()', (err, res) => { 
-if (err) throw err
-console.log(res)
-console.log("after")
-pgclient.end()
 });
-console.log("at the end")
+
+pgclient.connect((err) => {
+    if (err) {
+        console.error('Connection error', err.stack);
+    } else {
+        console.log('Connected successfully to PostgreSQL database');
+        // Proceed with the query only if the connection was successful
+        pgclient.query('SELECT NOW()', (err, res) => {
+            if (err) {
+                console.error('Query error', err);
+            } else {
+                console.log('Query result:', res.rows);
+            }
+            pgclient.end(); // Make sure to close the connection after the query
+        });
+    }
+});
