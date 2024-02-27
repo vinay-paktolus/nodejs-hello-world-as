c7fa182f75e6c9f71c9e76fe352d2d058b1b1d02
Arpan Solanki <solankiarpan@gmail.com>
2024-02-27 15:58:58 -0500

    feat: adding client.js

--- client.js
+++ client.js
@@ -7,15 +7,23 @@ const pgclient = new Client({
     password: 'postgres',
     database: 'postgres'
 });
-console.log("hello");
+
 pgclient.connect();
 
-console.log(pgclient.connect());
+const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
+const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
+const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
 
-pgclient.query('SELECT NOW()', (err, res) => {
+pgclient.query(table, (err, res) => {
     if (err) throw err
-    console.log(res)
-    pgclient.end()
 });
 
-console.log("at the end")
+pgclient.query(text, values, (err, res) => {
+    if (err) throw err
+});
+
+pgclient.query('SELECT * FROM student', (err, res) => {
+    if (err) throw err
+    console.log(err, res.rows) // Print the data in student table
+    pgclient.end()
+});
