cac4db7fcbeed6ee0e03726d82f00ea88527ac72
Arpan Solanki <solankiarpan@gmail.com>
2024-02-27 15:48:31 -0500

    feat: adding client.js

--- src/client.js
+++ src/client.js
@@ -1,12 +1,20 @@
-const { Client } = require('pg'); const pgclient = new Client({
-    host: process.env.POSTGRES_HOST, port: process.env.POSTGRES_PORT, user: 'postgres',
+const { Client } = require('pg'); 
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST, 
+    port: process.env.POSTGRES_PORT, 
+    user: 'postgres',
     password: 'postgres',
     database: 'postgres'
-}); pgclient.connect();
+}); 
+
+pgclient.connect();
+
 console.log("here")
+
 pgclient.query('SELECT NOW()', (err, res) => { 
 if (err) throw err
 console.log(res)
 console.log("after")
 pgclient.end()
 });
+console.log("at the end")
